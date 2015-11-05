120d87e4539ce9c3fa8a70a4f42fb27dc7741c78
janbean <janbean@163.com>
2015-11-05 15:42:24 +0800

    Add util createDeskShortCut

--- utils/ViewUtils.java
+++ utils/ViewUtils.java
@@ -18,6 +18,41 @@ public class ViewUtils {
 
 	private static final AtomicInteger sNextGeneratedId = new AtomicInteger(1);
 
+	/**
+     * 创建快捷方式
+     * 
+     * @param cxt
+     *            Context
+     * @param icon
+     *            快捷方式图标
+     * @param title
+     *            快捷方式标题
+     * @param cls
+     *            要启动的Activity类
+     */
+    public static void createDeskShortCut(Context cxt, int icon, String title,
+            Class<?> cls) {
+        // 创建快捷方式的Intent
+        Intent shortcutIntent = new Intent(
+                "com.android.launcher.action.INSTALL_SHORTCUT");
+        // 不允许重复创建
+        shortcutIntent.putExtra("duplicate", false);
+        // 需要现实的名称
+        shortcutIntent.putExtra(Intent.EXTRA_SHORTCUT_NAME, title);
+        // 快捷图片
+        Parcelable ico = Intent.ShortcutIconResource.fromContext(
+                cxt.getApplicationContext(), icon);
+        shortcutIntent.putExtra(Intent.EXTRA_SHORTCUT_ICON_RESOURCE, ico);
+        Intent intent = new Intent(cxt, cls);
+        // 下面两个属性是为了当应用程序卸载时桌面上的快捷方式会删除
+        intent.setAction("android.intent.action.MAIN");
+        intent.addCategory("android.intent.category.LAUNCHER");
+        // 点击快捷图片，运行的程序主入口
+        shortcutIntent.putExtra(Intent.EXTRA_SHORTCUT_INTENT, intent);
+        // 发送广播。OK
+        cxt.sendBroadcast(shortcutIntent);
+    }
+
 	public static int generateViewId() {
 		if (android.os.Build.VERSION.SDK_INT < 17 /* android.os.Build.VERSION_CODES.JELLY_BEAN_MR1 */) {
 			for (;;) {
