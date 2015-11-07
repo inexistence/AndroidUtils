e91406e09ca26816e118fb2aa39c952077579252
黄建斌 <inexistenceorz@gmail.com>
2015-11-08 00:05:06 +0800

    Refactor StringUtils
    Make friendlyTime(String) to friendlyTime(Date) and friendlyTime(String)

--- utils/StringUtils.java
+++ utils/StringUtils.java
@@ -200,6 +200,61 @@ public class StringUtils {
         }
     };
 
+    
+   public static String friendlyTime(Date time) {
+	   if (time == null) {
+           return "Unknown";
+       }
+	   if (!isInEasternEightZones()) {
+           time = transformTime(time, TimeZone.getTimeZone("GMT+08"),
+                   TimeZone.getDefault());
+       }
+       String ftime = "";
+       Calendar cal = Calendar.getInstance();
+
+       // 判断是否是同一天
+       String curDate = dateFormater2.get().format(cal.getTime());
+       String paramDate = dateFormater2.get().format(time);
+       if (curDate.equals(paramDate)) {
+           int hour = (int) ((cal.getTimeInMillis() - time.getTime()) / 3600000);
+           if (hour == 0)
+               ftime = Math.max(
+                       (cal.getTimeInMillis() - time.getTime()) / 60000, 1)
+                       + "分钟前";
+           else
+               ftime = hour + "小时前";
+           return ftime;
+       }
+
+       long lt = time.getTime() / 86400000;
+       long ct = cal.getTimeInMillis() / 86400000;
+       int days = (int) (ct - lt);
+       if (days == 0) {
+           int hour = (int) ((cal.getTimeInMillis() - time.getTime()) / 3600000);
+           if (hour == 0)
+               ftime = Math.max(
+                       (cal.getTimeInMillis() - time.getTime()) / 60000, 1)
+                       + "分钟前";
+           else
+               ftime = hour + "小时前";
+       } else if (days == 1) {
+           ftime = "昨天";
+       } else if (days == 2) {
+           ftime = "前天 ";
+       } else if (days > 2 && days < 31) {
+           ftime = days + "天前";
+       } else if (days >= 31 && days <= 2 * 31) {
+           ftime = "一个月前";
+       } else if (days > 2 * 31 && days <= 3 * 31) {
+           ftime = "2个月前";
+       } else if (days > 3 * 31 && days <= 4 * 31) {
+           ftime = "3个月前";
+       } else {
+           ftime = dateFormater2.get().format(time);
+       }
+       return ftime;
+   }
+    
     /**
      * 以友好的方式显示时间
      * 
@@ -207,62 +262,8 @@ public class StringUtils {
      * @return
      */
     public static String friendlyTime(String sdate) {
-        Date time = null;
-
-        if (isInEasternEightZones()) {
-            time = toDate(sdate);
-        } else {
-            time = transformTime(toDate(sdate), TimeZone.getTimeZone("GMT+08"),
-                    TimeZone.getDefault());
-        }
-
-        if (time == null) {
-            return "Unknown";
-        }
-        String ftime = "";
-        Calendar cal = Calendar.getInstance();
-
-        // 判断是否是同一天
-        String curDate = dateFormater2.get().format(cal.getTime());
-        String paramDate = dateFormater2.get().format(time);
-        if (curDate.equals(paramDate)) {
-            int hour = (int) ((cal.getTimeInMillis() - time.getTime()) / 3600000);
-            if (hour == 0)
-                ftime = Math.max(
-                        (cal.getTimeInMillis() - time.getTime()) / 60000, 1)
-                        + "分钟前";
-            else
-                ftime = hour + "小时前";
-            return ftime;
-        }
-
-        long lt = time.getTime() / 86400000;
-        long ct = cal.getTimeInMillis() / 86400000;
-        int days = (int) (ct - lt);
-        if (days == 0) {
-            int hour = (int) ((cal.getTimeInMillis() - time.getTime()) / 3600000);
-            if (hour == 0)
-                ftime = Math.max(
-                        (cal.getTimeInMillis() - time.getTime()) / 60000, 1)
-                        + "分钟前";
-            else
-                ftime = hour + "小时前";
-        } else if (days == 1) {
-            ftime = "昨天";
-        } else if (days == 2) {
-            ftime = "前天 ";
-        } else if (days > 2 && days < 31) {
-            ftime = days + "天前";
-        } else if (days >= 31 && days <= 2 * 31) {
-            ftime = "一个月前";
-        } else if (days > 2 * 31 && days <= 3 * 31) {
-            ftime = "2个月前";
-        } else if (days > 3 * 31 && days <= 4 * 31) {
-            ftime = "3个月前";
-        } else {
-            ftime = dateFormater2.get().format(time);
-        }
-        return ftime;
+        Date time = toDate(sdate);
+        return friendlyTime(time);
     }
 
     /**
