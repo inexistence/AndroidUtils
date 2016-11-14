5f6f31da88809d9aaeef443ddc313ce65d0ea2dc
黄建斌 <inexistenceorz@gmail.com>
2016-11-14 23:39:29 +0800

    1.error and json log;2.update function "getTag";3.add default tag header

--- utils/L.java
+++ utils/L.java
@@ -1,66 +1,134 @@
 package utils;
 
+import android.text.TextUtils;
 import android.util.Log;
+
+import com.jb.vmeeting.utils.SystemUtils;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
 
 /**
  * 全局log开关
  * 自动设置TAG为当前类名
  */
-public class L {
+public final class L {
 	public static boolean LOG_OPEN = true;
-	
-	public void setLogOpen(boolean open) {
+    public static final String TAG_HEAD = "JBAndroid";
+
+	public static void setLogOpen(boolean open) {
 		LOG_OPEN = open;
 	}
-	
-	private static String getTag() {
-        StackTraceElement stackTrace = Thread.currentThread().getStackTrace()[4];
 
-        String className = stackTrace.getClassName();
-        String tag = className.substring(className.lastIndexOf('.') + 1)
-                + "." + stackTrace.getMethodName() + "#" + stackTrace.getLineNumber();
-        return tag;
+    public static void v(String message, Object... args) {
+        if(!LOG_OPEN) return ;
+        Log.v(getTag(null), formatMessage(message, args));
     }
 
     public static void d(String message, Object... args) {
-    	if(!LOG_OPEN) return ;
-        message = formatMessage(message, args);
-        Log.d(getTag(), message);
+        if(!LOG_OPEN) return ;
+        Log.d(getTag(null), formatMessage(message, args));
     }
 
     public static void i(String message, Object... args) {
-    	if(!LOG_OPEN) return ;
-        message = formatMessage(message, args);
-        Log.i(getTag(), message);
-
+        if(!LOG_OPEN) return ;
+        Log.i(getTag(null), formatMessage(message, args));
     }
 
     public static void w(String message, Object... args) {
-    	if(!LOG_OPEN) return ;
-        message = formatMessage(message, args);
-        Log.w(getTag(), message);
+        if(!LOG_OPEN) return ;
+        Log.w(getTag(null), formatMessage(message, args));
     }
 
-    public static void w(Throwable e) {
-    	if(!LOG_OPEN) return ;
-        e.printStackTrace();
+    public static void e(Exception e) {
+        if(!LOG_OPEN) return ;
+        StringWriter sw = new StringWriter();
+        PrintWriter pw = new PrintWriter(sw);
+        e.printStackTrace(pw);
+        String stack = sw.toString();
+        Log.e(getTag(null), stack);
     }
-
     public static void e(String message, Object... args) {
-    	if(!LOG_OPEN) return ;
-        message = formatMessage(message, args);
-        Log.e(getTag(), message);
+        if(!LOG_OPEN) return ;
+        Log.e(getTag(null), formatMessage(message, args));
+    }
+    public static void v(String tag, String message, Object... args) {
+        if(!LOG_OPEN) return ;
+        Log.v(getTag(tag), formatMessage(message, args));
+    }
+
+    public static void d(String tag, String message, Object... args) {
+        if(!LOG_OPEN) return ;
+        Log.d(getTag(tag), formatMessage(message, args));
+    }
+
+    public static void i(String tag, String message, Object... args) {
+        if(!LOG_OPEN) return ;
+        Log.i(getTag(tag), formatMessage(message, args));
+    }
+
+    public static void w(String tag, String message, Object... args) {
+        if(!LOG_OPEN) return ;
+        Log.w(getTag(tag), formatMessage(message, args));
+    }
+
+    public static void e(String tag, String message, Object... args) {
+        if(!LOG_OPEN) return ;
+        Log.e(getTag(tag), formatMessage(message, args));
+    }
+    public static void w(Throwable e) {
+        if(!LOG_OPEN) return ;
+        StringWriter sw = new StringWriter();
+        PrintWriter pw = new PrintWriter(sw);
+        e.printStackTrace(pw);
+        String stack = sw.toString();
+        Log.w(getTag(null), stack);
     }
 
     public static void e(Throwable e) {
-    	if(!LOG_OPEN) return ;
-        e.printStackTrace();
+        if(!LOG_OPEN) return ;
+        StringWriter sw = new StringWriter();
+        PrintWriter pw = new PrintWriter(sw);
+        e.printStackTrace(pw);
+        String stack = sw.toString();
+        Log.e(getTag(null), stack);
     }
 
-    public static void v(String message, Object... args) {
-    	if(!LOG_OPEN) return ;
-        message = formatMessage(message, args);
-        Log.v(getTag(), message);
+    public static void json(String tag, String json) {
+        if(!LOG_OPEN) return ;
+        printJson(getTag(tag), json);
+    }
+
+    public static void json(String json) {
+        if(!LOG_OPEN) return ;
+        printJson(getTag(null), json);
+    }
+
+    public static void printJson(String tag, String json) {
+        if(TextUtils.isEmpty(json)) {
+            Log.e(tag, "Empty/Null json content");
+        } else {
+            try {
+                String message;
+                if(json.startsWith("{")) {
+                    JSONObject e1 = new JSONObject(json);
+                    message = "JSON:" + SystemUtils.getLineProperty() + e1.toString(4);
+                    Log.d(tag, message);
+                } else if(json.startsWith("[")) {
+                    JSONArray e = new JSONArray(json);
+                    message = "JSON:" + SystemUtils.getLineProperty() + e.toString(4);
+                    Log.d(tag, message);
+                } else {
+                    Log.e(tag, "Error json content");
+                }
+            } catch (JSONException var4) {
+                Log.e(tag, var4.getCause().getMessage() + "\n" + json);
+            }
+        }
     }
 
     private static String formatMessage(String message, Object... args) {
@@ -72,10 +140,17 @@ public class L {
                 return String.format(message, args);
             } catch (Exception e) {
                 e.printStackTrace();
-            } catch (OutOfMemoryError e) {
-                e.printStackTrace();
             }
         }
         return message;
+    }
+
+    private static String getTag(String customTag) {
+        StackTraceElement stackTrace = Thread.currentThread().getStackTrace()[4];
+        String className = stackTrace.getClassName();
+        String simpleClassName = className.substring(className.lastIndexOf('.') + 1);
+        String tag = TextUtils.isEmpty(customTag) ? TAG_HEAD : TAG_HEAD + "/" + customTag;
+        String position = " [("+simpleClassName.split("\\$")[0]+".java:"+stackTrace.getLineNumber()+")#"+stackTrace.getMethodName() +"]";
+        return tag + position;
     }
 }
